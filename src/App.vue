5f02b416bd98fc9e21b6364c101b3f18d0362212
Tania-aa <128038741+TaniaPYT@users.noreply.github.com>
2023-07-19 23:43:38 +0800

    main
    todoapp with vueify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,93 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            User Name
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Start Right Now!
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Vuetify Todo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+          { title: 'Todo', icon: 'mdi-checkbox-marked-circle-auto-outline', to:'/' },
+          { title: 'About', icon: 'mdi-help-box', to:'/about' },
+        ],
+    }),
   }
-}
-</style>
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima enim inventore suscipit perspiciatis libero, consequatur dolore magni ratione temporibus dolores, officiis quo non nisi esse, accusantium quis quam reiciendis animi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima enim inventore suscipit perspiciatis libero, consequatur dolore magni ratione temporibus dolores, officiis quo non nisi esse, accusantium quis quam reiciendis animi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima enim inventore suscipit perspiciatis libero, consequatur dolore magni ratione temporibus dolores, officiis quo non nisi esse, accusantium quis quam reiciendis animi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima enim inventore suscipit perspiciatis libero, consequatur dolore magni ratione temporibus dolores, officiis quo non nisi esse, accusantium quis quam reiciendis animi.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima enim inventore suscipit perspiciatis libero, consequatur dolore magni ratione temporibus dolores, officiis quo non nisi esse, accusantium quis quam reiciendis animi.
+    </p>
+  </div>
+</template>
